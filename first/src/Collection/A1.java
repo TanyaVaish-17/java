200039c61ac0506fd40280922cb9dcaf5b054f7e
TanyaVaish-17 <tanyavaish05@gmail.com>
2025-09-09 11:17:21 +0530

    Created A1 class file under Collection package

---
+++ first/src/Collection/A1.java
@@ -0,0 +1,14 @@
+package Collection;
+
+import java.util.ArrayList;
+
+public class A1 {
+    public static void main(String[] args) {
+        ArrayList l1= new ArrayList();
+        l1.add(10);
+        l1.add("Tanya");
+        l1.add(false);
+        l1.add(17.56);
+        System.out.println(l1);
+    }
+}
--- first/src/backend/Exception1.java
+++ first/src/backend/Exception1.java
@@ -1,10 +1,11 @@
 package backend;
-
+//an object is created by the main memory which includes exception, description, stack track
+//if the exception is not been handled ,jvm will print the message called as default exception handler.
 import java.io.*;
-
+// we can write t/c/f inside try, catch and final all.
 public class Exception1 {
     public static void main(String[] args) {
-        try {
+        try { //risky code
             FileReader fr = new FileReader("file.txt");
         } catch (FileNotFoundException e) {
             System.out.println("Checked Exception handled: File not found.");
